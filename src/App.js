0eed04489e4fb964f7212621676426b9bad6ac19
Niklas Burg <niklasburg1999.nb@gmail.com>
2023-03-15 16:45:18 +0100

    initial setup with basic routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Summary from "./pages/Summary";
+import Contacts from "./pages/Contacts";
+import Board from "./pages/Board";
+import AddTask from "./pages/AddTask";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Summary />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/add" element={<AddTask />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Summary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Summary = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Summary;
